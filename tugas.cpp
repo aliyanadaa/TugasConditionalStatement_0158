5cca38e41c6c12557efc0220d1f331b0490efb42
aliyanadaa <aliyanadaq@gmail.com>
2026-03-09 23:17:26 +0700

    Menambahkan fungsi statusBMI

--- tugas.cpp
+++ tugas.cpp
@@ -11,3 +11,15 @@ void inputData(double &berat, double &tinggi) {
 double hitungBMI(double berat, double tinggi) {
     return berat / (tinggi * tinggi);
 }
+
+string statusBMI(double bmi) {
+    if (bmi < 18.5) {
+        return "Berat Badan Kurang";
+    } else if (bmi < 25) {
+        return "Berat Badan Normal";
+    } else if (bmi < 30) {
+        return "Berat Badan Kelebihan";
+    } else {
+        return "Obesitas";
+    }
+}
