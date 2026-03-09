5d31fa8be1df89cf1257ce13f085361eedd6f6ef
aliyanadaa <aliyanadaq@gmail.com>
2026-03-09 23:16:49 +0700

    Menambahkan fungsi hitungBMI

--- tugas.cpp
+++ tugas.cpp
@@ -7,3 +7,7 @@ void inputData(double &berat, double &tinggi) {
     cout << "Masukkan tinggi badan (meter): ";
     cin >> tinggi;
 }
+
+double hitungBMI(double berat, double tinggi) {
+    return berat / (tinggi * tinggi);
+}
